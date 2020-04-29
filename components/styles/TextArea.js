ae3dfa295bb96d47dbf9440b02edf72cfff13fd9
daniel lopez <dlopez807@gmail.com>
2020-04-29 11:31:57 -0700

    add footer and custom themes

--- components/styles/TextArea.js
+++ components/styles/TextArea.js
@@ -1,30 +1,19 @@
 import styled from 'styled-components';
 
 export default styled.textarea`
+  background: ${props => props.theme.background};
+  color: ${props => props.theme.color};
+  width: 100%;
+  height: 100%;
+  font-size: 3vh;
+  font-family: Arial, Helvetica;
+  padding: 5px;
   border: none !important;
   overflow: auto;
   outline: none !important;
   -webkit-box-shadow: none;
   -moz-box-shadow: none;
   box-shadow: none;
-
-  width: 100%;
-  height: 100vh;
-  font-size: 3vh;
-  font-family: Arial, Helvetica;
-  padding: 5px;
-  box-sizing: border-box;
-  border: none;
   overflow-y: scroll;
   -webkit-overflow-scrolling: touch;
-
-  &.dark {
-    background: ${props => props.theme.dark.background};
-    color: ${props => props.theme.dark.color};
-  }
-
-  &.cobalt {
-    background: ${props => props.theme.cobalt.background};
-    color: ${props => props.theme.cobalt.color};
-  }
 `;
