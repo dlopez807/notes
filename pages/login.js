d50ca2740f6c763c4009f20dbb4c935bd8da121e
daniel lopez <dlopez807@gmail.com>
2020-06-09 07:44:45 -0700

    add link on login to go back home

--- pages/login.js
+++ pages/login.js
@@ -1,10 +1,25 @@
+import Link from 'next/link'
+import { X } from 'react-feather'
+
 import Page from '../components/Page'
 import Login from '../components/Login'
+import Footer from '../components/styles/Footer'
 
 export default () => (
   <Page title="login">
     <main>
       <Login />
+      <Footer>
+        <ul>
+          <li>
+            <Link href="/">
+              <a>
+                <X />
+              </a>
+            </Link>
+          </li>
+        </ul>
+      </Footer>
     </main>
   </Page>
 )
