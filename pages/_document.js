a1d767a3cc70c2c10de8e7f5e7e32f6b80e87436
daniel lopez <dlopez807@gmail.com>
2020-09-06 23:20:20 -0700

    update icon

--- pages/_document.js
+++ pages/_document.js
@@ -39,19 +39,11 @@ export default class MyDocument extends Document {
           />
           <meta name="description" content="notes" />
           <meta charSet="utf-8" />
-          <link rel="shortcut icon" href="/favicon.ico" />
-          <link rel="apple-touch-icon" sizes="57x57" href="/logo-57x57.png" />
-          <link rel="apple-touch-icon" sizes="72x72" href="/logo-72x72.png" />
-          <link
-            rel="apple-touch-icon"
-            sizes="114x114"
-            href="/logo-114x114.png"
-          />
-          <link
-            rel="apple-touch-icon"
-            sizes="144x144"
-            href="/logo-144x144.png"
-          />
+          <link rel="shortcut icon" href="/favicon.png" />
+          <link rel="apple-touch-icon" sizes="57x57" href="/icon.png" />
+          <link rel="apple-touch-icon" sizes="72x72" href="/icon.png" />
+          <link rel="apple-touch-icon" sizes="114x114" href="/icon.png" />
+          <link rel="apple-touch-icon" sizes="144x144" href="/icon.png" />
           <link rel="manifest" href="/manifest.json" />
         </Head>
         <body>
