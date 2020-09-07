79677c6952396091fe6e97cfe863c2bd6927fd2c
daniel lopez <dlopez807@gmail.com>
2020-09-06 23:15:23 -0700

    update icon

--- components/Meta.js
+++ components/Meta.js
@@ -5,14 +5,17 @@ const Meta = () => (
     <meta name="mobile-web-app-capable" content="yes" />
     <meta name="apple-mobile-web-app-capable" content="yes" />
     <meta name="msapplication-starturl" content="/" />
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+    <meta
+      name="viewport"
+      content="width=device-width, initial-scale=1, shrink-to-fit=no"
+    />
     <meta name="description" content="notes" />
     <meta charSet="utf-8" />
-    <link rel="shortcut icon" href="/favicon.ico" />
-    <link rel="apple-touch-icon" sizes="57x57" href="/logo-57x57.png" />
-    <link rel="apple-touch-icon" sizes="72x72" href="/logo-72x72.png" />
-    <link rel="apple-touch-icon" sizes="114x114" href="/logo-114x114.png" />
-    <link rel="apple-touch-icon" sizes="144x144" href="/logo-144x144.png" />
+    <link rel="shortcut icon" href="/favicon.png" />
+    <link rel="apple-touch-icon" sizes="57x57" href="/icon.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="/icon.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="/icon.png" />
+    <link rel="apple-touch-icon" sizes="144x144" href="/icon.png" />
     <link rel="manifest" href="/manifest.json" />
     <title>notes</title>
   </Head>
